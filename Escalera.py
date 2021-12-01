efad461273dbb18572325390104d410446a36e94
Carlos.17 <rangelrodriguez.17>
2021-12-01 14:23:15 -0500

    Se deja unicamente escalones

--- Escalera.py
+++ Escalera.py
@@ -4,6 +4,6 @@ def escalones(num):
     return escalones(num - 1) + escalones(num - 2)
 def cuentamane(s):
     return escalones(s + 1)
-Num = int(input('Ingrese numero de escalones:'))
+Num = int(input('escalones:'))
 print('cantidad de maneras para subir la escalera:')
 print(cuentamane(num))
